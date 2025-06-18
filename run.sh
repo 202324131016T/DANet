0d2d78f9548a9df139548d43a3b90fa2c0e62ec9
202324131016T <143988176+202324131016T@users.noreply.github.com>
2025-06-18 18:22:26 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ model_name=ours
 for Recurrence in 1 2 3; do
 #  for image_encoder in ${image_encoder_list[@]}; do
   for image_encoder in densenet121; do
-    path_dir=${model_name}-${dataset}-${image_encoder}
+    path_dir=${model_name}-${dataset}-${image_encoder}-${Recurrence}
     CUDA_VISIBLE_DEVICES=${GPU_ID} python main.py --path_dir ${path_dir} --init_method ${init_method} --dataset ${dataset} --image_encoder ${image_encoder} --max_epochs ${max_epochs} --batch_size ${batch_size} --lr ${lr}
     for pt_file in ./result/${path_dir}/*.pt; do
       CUDA_VISIBLE_DEVICES=${GPU_ID} python inference.py --path_dir ${path_dir}  --pt_path ${pt_file} --dataset ${dataset} --image_encoder ${image_encoder} --aggregation_method ${aggregation_method} --top_K ${top_K}
@@ -39,9 +39,10 @@ path_dir=None
 pt_file=None
 dataset=her2st  # "GSE240429", "her2st"
 model_name=ours
+for Recurrence in 1 2 3; do
 #for image_encoder in ${image_encoder_list[@]}; do
 for image_encoder in densenet121; do
-  path_dir=${model_name}-${dataset}-${image_encoder}
+  path_dir=${model_name}-${dataset}-${image_encoder}-${Recurrence}
   CUDA_VISIBLE_DEVICES=${GPU_ID} python main.py --path_dir ${path_dir} --init_method ${init_method} --dataset ${dataset} --image_encoder ${image_encoder} --max_epochs ${max_epochs} --batch_size ${batch_size} --lr ${lr}
   for pt_file in ./result/${path_dir}/*.pt; do
     for fold in 0 6 12 18 24 27 31 33; do
@@ -49,23 +50,28 @@ for image_encoder in densenet121; do
     done
   done
 done
+done
 
 
 # HisToGene her2st
 cd ./method/HisToGene/
-ckpt_name=../../checkpoint/HisToGene/her2st_HisToGene_best
+for Recurrence in 1 2 3; do
+ckpt_name=../../checkpoint/HisToGene/her2st_HisToGene_best-${Recurrence}
 ckpt=${ckpt_name}.ckpt
 log=${ckpt_name}.txt
 CUDA_VISIBLE_DEVICES=${GPU_ID} python ./main_HisToGene.py --train train --ckpt ${ckpt} >> ${log}
+done
 cd ../../
 #exit 0
 
 # ST-Net her2st
 cd ./method/ST-Net/
-ckpt_name=../../checkpoint/ST-Net/her2st_ST-Net_best
+for Recurrence in 1 2 3; do
+ckpt_name=../../checkpoint/ST-Net/her2st_ST-Net_best-${Recurrence}
 ckpt=${ckpt_name}.ckpt
 log=${ckpt_name}.txt
 CUDA_VISIBLE_DEVICES=${GPU_ID} python ./main_ST-Net.py --train train --ckpt ${ckpt} >> ${log}
+done
 cd ../../
 #exit 0
 
@@ -79,6 +85,7 @@ path_dir=None
 pt_file=None
 dataset=her2st  # "GSE240429", "her2st"
 model_name=BLEEP
+for Recurrence in 1 2 3; do
 #for image_encoder in ${image_encoder_list[@]}; do
 for image_encoder in resnet50; do
   path_dir=${model_name}-${dataset}-${image_encoder}-${Recurrence}
@@ -88,4 +95,5 @@ for image_encoder in resnet50; do
       CUDA_VISIBLE_DEVICES=${GPU_ID} python inference.py --path_dir ${path_dir}  --pt_path ${pt_file} --dataset ${dataset} --fold ${fold} --image_encoder ${image_encoder} --aggregation_method ${aggregation_method} --top_K ${top_K}
     done
   done
+done
 done
