61824631c7d970354731d8dd1dbcc9f46045d8b3
202324131016T <143988176+202324131016T@users.noreply.github.com>
2025-06-18 18:18:50 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,7 +20,7 @@ model_name=ours
 for Recurrence in 1 2 3; do
 #  for image_encoder in ${image_encoder_list[@]}; do
   for image_encoder in densenet121; do
-    path_dir=${model_name}-${dataset}-${image_encoder}-${Recurrence}
+    path_dir=${model_name}-${dataset}-${image_encoder}
     CUDA_VISIBLE_DEVICES=${GPU_ID} python main.py --path_dir ${path_dir} --init_method ${init_method} --dataset ${dataset} --image_encoder ${image_encoder} --max_epochs ${max_epochs} --batch_size ${batch_size} --lr ${lr}
     for pt_file in ./result/${path_dir}/*.pt; do
       CUDA_VISIBLE_DEVICES=${GPU_ID} python inference.py --path_dir ${path_dir}  --pt_path ${pt_file} --dataset ${dataset} --image_encoder ${image_encoder} --aggregation_method ${aggregation_method} --top_K ${top_K}
